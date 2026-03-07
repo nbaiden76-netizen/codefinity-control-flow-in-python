6e36f071b22b81a77d33fde30adfa28eff66ecc3
nbaiden76-netizen <>
2026-03-07 11:12:36 +0000

    Comparison operators

---
+++ mastering_python_if_statements/comparison_operators/main.py
@@ -0,0 +1,32 @@
+street_temperature = 15
+hydration_goal_met = True
+
+running_temp = False
+
+if 10 <= street_temperature <= 20:
+    running_temp = True
+
+# Testing
+print("Are the conditions ideal for running now?", running_temp)
+
+# Hydration goal check
+if hydration_goal_met:
+   print("Great job meeting your hydration goal!")
+
+#favorite_activity = "Yoga"
+#current_activity = "Running"
+
+#if favorite_activity != current_activity:
+ #   print("This is not your favorite activity, but keep going!")
+
+# Check if the user completed their morning routine
+#morning_run_completed = True
+
+#if morning_run_completed:
+#    print("Great job on completing your morning run!")
+
+#daily_steps = 7500
+
+# Step count range check
+#if 5000 <= daily_steps <= 10000:
+ #   print("You're on track with your step count!")
