c898dba861115b04c925aab3f24ebcb193cd3140
nbaiden76-netizen <>
2026-03-07 10:53:08 +0000

    If statements

---
+++ mastering_python_if_statements/if_statements/main.py
@@ -0,0 +1,24 @@
+calories_burned = 200
+calories_goal = 300
+
+calories_goal_met = False  # default value
+
+# Check calories
+if calories_burned >= calories_goal:
+    calories_goal_met = True
+
+# Testing
+print("Is the goal for burned calories met?", calories_goal_met)
+
+#steps_taken = 7500
+#step_goal = 10000
+
+#if steps_taken < step_goal:
+ #   print(f"Keep going! You need {step_goal - steps_taken} more steps to reach your goal.")
+
+#steps_taken = 10000
+#step_goal = 10000
+
+#if steps_taken < step_goal:
+ #   print(f"Keep going! You need {step_goal - steps_taken} more steps to reach your goal.")
+
